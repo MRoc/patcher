49d7a0202fd765e18343736d9c7ae364a8a75abb
Matthias Mayrock <mma@templafy.com>
2021-08-16 19:44:39 +0200

    Latest alignment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import { OpType, OpTypes, getValue } from "./optype";
 
-const type = new OpType();
-
 export { OpType, getValue };
 
 export function emptyHistory() {
@@ -16,37 +14,41 @@ export const defaultTransaction = -1;
 
 export const defaultVersion = 0;
 
-export function patch(state, op, newTransaction) {
-  return patchWithOps(state, op, newTransaction)[0];
+export function Patcher(type) {
+  this.type = type;
 }
 
-export function patchWithOps(state, op, newTransaction) {
+Patcher.prototype.patch = function (state, op, newTransaction) {
+  return this.patchWithOps(state, op, newTransaction)[0];
+};
+
+Patcher.prototype.patchWithOps = function (state, op, newTransaction) {
   let history = state.history || emptyHistory();
   let transaction =
     state.transaction === undefined ? defaultTransaction : state.transaction;
   if (newTransaction || transaction === defaultTransaction) {
-    if (canMergeOp(history, transaction, op)) {
-      history = discardFutureOps(history, transaction + 1);
-      history = mergeLastOp(history, op);
+    if (this.canMergeOp(history, transaction, op)) {
+      history = this.discardFutureOps(history, transaction + 1);
+      history = this.mergeLastOp(history, op);
     } else {
       transaction++;
-      history = discardFutureOps(history, transaction);
-      history = insertOp(state, history, transaction, op);
+      history = this.discardFutureOps(history, transaction);
+      history = this.insertOp(state, history, transaction, op);
     }
   } else {
-    history = insertOp(state, history, transaction, op);
+    history = this.insertOp(state, history, transaction, op);
   }
 
   return [
     combine(
-      type.apply(state, op),
+      this.type.apply(state, op),
       history,
       transaction,
       nextVersion(state, op)
     ),
     op,
   ];
-}
+};
 
 export function combine(
   state,
@@ -74,15 +76,15 @@ function nextVersion(state, op) {
   return state.version + op.length;
 }
 
-export function hasUndo(state) {
+Patcher.prototype.hasUndo = function (state) {
   return state.history.ops.length > 0 && state.transaction > defaultTransaction;
-}
+};
 
-export function undo(state) {
-  return undoWithOps(state)[0];
-}
+Patcher.prototype.undo = function (state) {
+  return this.undoWithOps(state)[0];
+};
 
-export function undoWithOps(state) {
+Patcher.prototype.undoWithOps = function (state) {
   const transaction = state.transaction;
 
   const operations = state.history.opsInverted
@@ -95,27 +97,27 @@ export function undoWithOps(state) {
 
   return [
     combine(
-      type.apply(state, operations),
+      this.type.apply(state, operations),
       state.history,
       transaction - 1,
       nextVersion(state, operations)
     ),
     operations,
   ];
-}
+};
 
-export function hasRedo(state) {
+Patcher.prototype.hasRedo = function (state) {
   return (
     state.history.ops.length > 0 &&
     state.transaction < arrayMax(state.history.ops.map((op) => op.transaction))
   );
-}
+};
 
-export function redo(state) {
-  return redoWithOps(state)[0];
-}
+Patcher.prototype.redo = function (state) {
+  return this.redoWithOps(state)[0];
+};
 
-export function redoWithOps(state) {
+Patcher.prototype.redoWithOps = function (state) {
   const transaction = state.transaction + 1;
 
   const operations = state.history.ops.filter(
@@ -128,16 +130,16 @@ export function redoWithOps(state) {
 
   return [
     combine(
-      type.apply(state, operations),
+      this.type.apply(state, operations),
       state.history,
       transaction,
       nextVersion(state, operations)
     ),
     operations,
   ];
-}
+};
 
-export function canMergeOp(history, transaction, op) {
+Patcher.prototype.canMergeOp = function (history, transaction, op) {
   if (Array.isArray(op)) {
     if (op.length === 1) {
       op = op[0];
@@ -161,9 +163,9 @@ export function canMergeOp(history, transaction, op) {
   }
 
   return true;
-}
+};
 
-export function mergeLastOp(history, op) {
+Patcher.prototype.mergeLastOp = function (history, op) {
   if (Array.isArray(op)) {
     if (op.length !== 1) {
       throw new Error(`Merge only works on single operations!`);
@@ -177,18 +179,18 @@ export function mergeLastOp(history, op) {
     ops: [...arraySkipLast(history.ops), { ...lastOp, value: op.value }],
     opsInverted: history.opsInverted,
   };
-}
+};
 
-export function discardFutureOps(history, transaction) {
+Patcher.prototype.discardFutureOps = function (history, transaction) {
   return {
     ops: history.ops.filter((op) => op.transaction < transaction),
     opsInverted: history.opsInverted.filter(
       (op) => op.transaction < transaction
     ),
   };
-}
+};
 
-export function insertOp(state, history, transaction, op) {
+Patcher.prototype.insertOp = function (state, history, transaction, op) {
   if (!Array.isArray(op)) {
     op = [op];
   }
@@ -198,14 +200,14 @@ export function insertOp(state, history, transaction, op) {
   });
 
   const opsInverted = op.map((o) => {
-    return { ...type.invertWithDoc(o, state), transaction };
+    return { ...this.type.invertWithDoc(o, state), transaction };
   });
 
   return {
     ops: [...history.ops, ...ops],
     opsInverted: [...history.opsInverted, ...opsInverted],
   };
-}
+};
 
 function arrayLast(array) {
   return array[array.length - 1];
--- src/index.test.js
+++ src/index.test.js
@@ -1,39 +1,29 @@
-import {
-  patch,
-  undo,
-  redo,
-  combine,
-  canMergeOp,
-  mergeLastOp,
-  discardFutureOps,
-  emptyHistory,
-  createHistory,
-} from "./index.js";
-
+import { Patcher, combine, emptyHistory, createHistory } from "./index.js";
 import { OpType } from "./optype.js";
 
 const type = new OpType();
+const patcher = new Patcher(type);
 
 describe("canMergeOp", () => {
   test("With no last operations returns false", () => {
     const history = emptyHistory();
     const transaction = -1;
     const operation = type.replaceOp(["a"], 0, 1);
-    const canMerge = canMergeOp(history, transaction, operation);
+    const canMerge = patcher.canMergeOp(history, transaction, operation);
     expect(canMerge).toBe(false);
   });
   test("With last operation not being replace returns false", () => {
     const history = createHistory([type.insertOp(["a", "value"], "b", 0)]);
     const transaction = 0;
     const operation = type.replaceOp(["a"], 0, 1);
-    const canMerge = canMergeOp(history, transaction, operation);
+    const canMerge = patcher.canMergeOp(history, transaction, operation);
     expect(canMerge).toBe(false);
   });
   test("With last operation path not equal returns false", () => {
     const history = createHistory([type.replaceOp(["b"], 1, 0)]);
     const transaction = 0;
     const operation = type.replaceOp(["a"], 0, 1);
-    const canMerge = canMergeOp(history, transaction, operation);
+    const canMerge = patcher.canMergeOp(history, transaction, operation);
     expect(canMerge).toBe(false);
   });
   test("With two operations returns false", () => {
@@ -43,21 +33,21 @@ describe("canMergeOp", () => {
       type.replaceOp(["a"], 0, 1),
       type.replaceOp(["b"], 0, 1),
     ];
-    const canMerge = canMergeOp(history, transaction, operation);
+    const canMerge = patcher.canMergeOp(history, transaction, operation);
     expect(canMerge).toBe(false);
   });
   test("With replace on same path and single op returns true", () => {
     const history = createHistory([type.replaceOp(["b", "c"], 1, 0)]);
     const transaction = 0;
     const operation = type.replaceOp(["b", "c"], 2, 0);
-    const canMerge = canMergeOp(history, transaction, operation);
+    const canMerge = patcher.canMergeOp(history, transaction, operation);
     expect(canMerge).toBe(true);
   });
   test("With replace on same path in array and single op returns true", () => {
     const history = createHistory([type.replaceOp(["b", "c"], 1, 0)]);
     const transaction = 0;
     const operation = [type.replaceOp(["b", "c"], 2, 0)];
-    const canMerge = canMergeOp(history, transaction, operation);
+    const canMerge = patcher.canMergeOp(history, transaction, operation);
     expect(canMerge).toBe(true);
   });
 });
@@ -66,7 +56,7 @@ describe("mergeLastOp", () => {
   test("With last operation merge-able, overwrites value", () => {
     const history = createHistory([type.replaceOp(["a", "b"], 1, 0)]);
     const operation = type.replaceOp(["a", "b"], 2, 0);
-    const mergedHistory = mergeLastOp(history, operation);
+    const mergedHistory = patcher.mergeLastOp(history, operation);
     expect(mergedHistory).toStrictEqual(
       createHistory([type.replaceOp(["a", "b"], 2, 0)])
     );
@@ -87,7 +77,7 @@ describe("discardFutureOps", () => {
         type.replaceOp(["a", "b"], 0, 2),
       ]
     );
-    const newHistory = discardFutureOps(history, 1);
+    const newHistory = patcher.discardFutureOps(history, 1);
     expect(newHistory).toStrictEqual(
       createHistory([history.ops[0]], [history.opsInverted[0]])
     );
@@ -98,7 +88,7 @@ describe("patch", () => {
   test("With add to object, starts transaction, adds to history, adds property", () => {
     const state = {};
     const op = type.insertOp(["a"], 2);
-    const clone = patch(state, op);
+    const clone = patcher.patch(state, op);
     expect(clone).toStrictEqual(
       combine(
         { a: 2 },
@@ -111,7 +101,7 @@ describe("patch", () => {
   test("With add to array, starts transaction, adds to history, adds element", () => {
     const state = [1, 2, 3];
     const op = type.insertOp([1], 5);
-    const clone = patch(state, op);
+    const clone = patcher.patch(state, op);
     expect(clone.slice(0)).toStrictEqual([1, 5, 2, 3]);
     expect(clone.history.ops.length).toBe(1);
     expect(clone.history.opsInverted.length).toBe(1);
@@ -120,7 +110,7 @@ describe("patch", () => {
   test("With add-range to array, starts transaction, adds to history, adds elements", () => {
     const state = [1, 2, 3];
     const op = type.insertRangeOp([1], [4, 5]);
-    const clone = patch(state, op);
+    const clone = patcher.patch(state, op);
     expect(clone.slice(0)).toStrictEqual([1, 4, 5, 2, 3]);
     expect(clone.history.ops.length).toBe(1);
     expect(clone.history.opsInverted.length).toBe(1);
@@ -129,7 +119,7 @@ describe("patch", () => {
   test("With remove from object, starts transaction, adds to history, removes property", () => {
     const state = { a: 2 };
     const op = type.removeOp(["a"]);
-    const clone = patch(state, op);
+    const clone = patcher.patch(state, op);
     expect(clone).toStrictEqual(
       combine(
         {},
@@ -142,7 +132,7 @@ describe("patch", () => {
   test("With remove from array, starts transaction, adds to history, removes element", () => {
     const state = [1, 2, 5, 3];
     const op = type.removeOp([2]);
-    const clone = patch(state, op);
+    const clone = patcher.patch(state, op);
     expect(clone.slice(0)).toStrictEqual([1, 2, 3]);
     expect(clone.history.ops.length).toBe(1);
     expect(clone.history.opsInverted.length).toBe(1);
@@ -151,7 +141,7 @@ describe("patch", () => {
   test("With remove-range from array, starts transaction, adds to history, removes elements", () => {
     const state = [1, 2, 5, 3];
     const op = type.removeRangeOp([{ index: 1, length: 2 }]);
-    const clone = patch(state, op);
+    const clone = patcher.patch(state, op);
     expect(clone.slice(0)).toStrictEqual([1, 3]);
     expect(clone.history.ops.length).toBe(1);
     expect(clone.history.opsInverted.length).toBe(1);
@@ -171,7 +161,7 @@ describe("patch", () => {
   test("With replace on object, starts transaction, adds to history, updates property", () => {
     const state = { a: 2 };
     const op = type.replaceOp(["a"], 3);
-    const clone = patch(state, op);
+    const clone = patcher.patch(state, op);
     expect(clone).toStrictEqual(
       combine(
         { a: 3 },
@@ -187,14 +177,14 @@ describe("patch", () => {
   test("With replace on array, starts transaction, adds to history, updates element", () => {
     const state = [1, 2, 5, 3];
     const op = type.replaceOp([2], 4);
-    const clone = patch(state, op);
+    const clone = patcher.patch(state, op);
     expect(clone.slice(0)).toStrictEqual([1, 2, 4, 3]);
     expect(clone.history.ops.length).toBe(1);
     expect(clone.history.opsInverted.length).toBe(1);
     expect(clone.transaction).toBe(0);
   });
   test("With two sets, increments version twice", () => {
-    const clone = patch({}, [type.insertOp(["a"], 2), type.insertOp(["a"], 2)]);
+    const clone = patcher.patch({}, [type.insertOp(["a"], 2), type.insertOp(["a"], 2)]);
     expect(clone.version).toBe(2);
   });
 });
@@ -206,7 +196,7 @@ describe("undo", () => {
       [type.removeOp(["a"], 0)]
     );
     const state = combine({ a: 2 }, history, 0);
-    const clone = undo(state);
+    const clone = patcher.undo(state);
     expect(clone).toStrictEqual(combine({}, history, -1, 1));
   });
   test("With add to array, removes element and decrements transaction", () => {
@@ -215,7 +205,7 @@ describe("undo", () => {
       [type.removeOp([2], 0)]
     );
     const state = combine([1, 2, 5, 3], history, 0);
-    const clone = undo(state);
+    const clone = patcher.undo(state);
     expect(clone.slice(0)).toStrictEqual([1, 2, 3]);
   });
   test("With add-range to array,removes elements and decrements transaction", () => {
@@ -224,7 +214,7 @@ describe("undo", () => {
       [type.removeRangeOp([{ index: 1, length: 2 }], 0)]
     );
     const state = combine([1, 4, 5, 2, 3], history, 0);
-    const clone = undo(state);
+    const clone = patcher.undo(state);
     expect(clone.slice(0)).toStrictEqual([1, 2, 3]);
   });
   test("With remove from object, adds property and decrements transaction", () => {
@@ -233,7 +223,7 @@ describe("undo", () => {
       [type.insertOp(["a"], 2, 0)]
     );
     const state = combine({}, history, 0);
-    const clone = undo(state);
+    const clone = patcher.undo(state);
     expect(clone).toStrictEqual(combine({ a: 2 }, history, -1, 1));
   });
   test("With remove from array, adds element and decrements transaction", () => {
@@ -242,7 +232,7 @@ describe("undo", () => {
       [type.insertOp([2], 5, 0)]
     );
     const state = combine([1, 2, 3], history, 0);
-    const clone = undo(state);
+    const clone = patcher.undo(state);
     expect(clone.slice(0)).toStrictEqual([1, 2, 5, 3]);
   });
   test("With remove-range from array, adds elements and decrements transaction", () => {
@@ -251,7 +241,7 @@ describe("undo", () => {
       [type.insertRangeOp([1], [2, 5], 0)]
     );
     const state = combine([1, 3], history, 0);
-    const clone = undo(state);
+    const clone = patcher.undo(state);
     expect(clone.slice(0)).toStrictEqual([1, 2, 5, 3]);
   });
   test("With replace on object, updates property and decrements transaction", () => {
@@ -260,7 +250,7 @@ describe("undo", () => {
       [type.replaceOp(["a"], 2, 0)]
     );
     const state = combine({ a: 3 }, history, 0);
-    const clone = undo(state);
+    const clone = patcher.undo(state);
     expect(clone).toStrictEqual(combine({ a: 2 }, history, -1, 1));
   });
   test("With replace on array, updates element and decrements transaction", () => {
@@ -269,7 +259,7 @@ describe("undo", () => {
       [type.replaceOp([1], 2, 0)]
     );
     const state = combine([1, 4, 3], history, 0);
-    const clone = undo(state);
+    const clone = patcher.undo(state);
     expect(clone.slice(0)).toStrictEqual([1, 2, 3]);
   });
 });
@@ -281,7 +271,7 @@ describe("redo", () => {
       [type.removeOp(["a"], 0)]
     );
     const state = combine({}, history, -1);
-    const clone = redo(state);
+    const clone = patcher.redo(state);
     expect(clone).toStrictEqual(combine({ a: 2 }, history, 0, 1));
   });
   test("With add to array, add element and increments transaction", () => {
@@ -290,7 +280,7 @@ describe("redo", () => {
       [type.removeOp([2], 0)]
     );
     const state = combine([1, 2, 3], history, -1);
-    const clone = redo(state);
+    const clone = patcher.redo(state);
     expect(clone.slice(0)).toStrictEqual([1, 2, 4, 3]);
   });
   test("With add-range to array, adds elements and increments transaction", () => {
@@ -299,7 +289,7 @@ describe("redo", () => {
       [type.removeRangeOp([{ index: 1, length: 2 }], 0)]
     );
     const state = combine([1, 2, 3], history, -1);
-    const clone = redo(state);
+    const clone = patcher.redo(state);
     expect(clone.slice(0)).toStrictEqual([1, 4, 5, 2, 3]);
   });
   test("With remove from object, removes property and increments transaction", () => {
@@ -308,7 +298,7 @@ describe("redo", () => {
       [type.insertOp(["a"], 2, 0)]
     );
     const state = combine({ a: 2 }, history, -1);
-    const clone = redo(state);
+    const clone = patcher.redo(state);
     expect(clone).toStrictEqual(combine({}, history, 0, 1));
   });
   test("With remove from array, removes element and increments transaction", () => {
@@ -317,7 +307,7 @@ describe("redo", () => {
       [type.insertOp([2], 4, 0)]
     );
     const state = combine([1, 2, 4, 3], history, -1);
-    const clone = redo(state);
+    const clone = patcher.redo(state);
     expect(clone.slice(0)).toStrictEqual([1, 2, 3]);
   });
   test("With replace on object, updates property and increments transaction", () => {
@@ -326,7 +316,7 @@ describe("redo", () => {
       [type.replaceOp(["a"], 2, 0)]
     );
     const state = combine({ a: 2 }, history, -1);
-    const clone = redo(state);
+    const clone = patcher.redo(state);
     expect(clone).toStrictEqual(combine({ a: 3 }, history, 0, 1));
   });
   test("With replace on array, updates element and increments transaction", () => {
@@ -335,7 +325,7 @@ describe("redo", () => {
       [type.replaceOp([1], 2, 0)]
     );
     const state = combine([1, 2, 3], history, -1);
-    const clone = redo(state);
+    const clone = patcher.redo(state);
     expect(clone.slice(0)).toStrictEqual([1, 5, 3]);
   });
 });
